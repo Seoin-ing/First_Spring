78f7d432827c7e3ab8ba1312014c3eabcbd76ff0
Seoin-ing <aadd20166@gmail.com>
2026-03-11 22:18:16 +0900

    3일차 - Member Class, Json 응답, PathVariable

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -2,7 +2,10 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.Member;
 
 
 @RestController
@@ -18,4 +21,13 @@ public class HelloController {
         return "서인씨 잘가요!";
     }
     
+    @GetMapping("/member")
+    public Member getMember() {
+        return new Member("박서인", 27);
+    }
+    
+    @GetMapping("/member/{name}")
+    public Member getMemberByName(@PathVariable String name) {
+        return new Member(name, 0);
+    }
 }
